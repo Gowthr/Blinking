87f258ffc0a3ae21b10f9d528ee4c86b8e7a6e81
Gowthr <109998596+Gowthr@users.noreply.github.com>
2023-10-23 05:28:58 -0400

    Update Blinking.java

--- src/main/java/me/gowthr/blinking/Blinking.java
+++ src/main/java/me/gowthr/blinking/Blinking.java
@@ -1,7 +1,5 @@
 package me.gowthr.blinking;
-
 import org.bukkit.plugin.java.JavaPlugin;
-
 public final class Blinking extends JavaPlugin {
     @Override
     public void onEnable() {runnable runnable = new runnable(this);runnable.runTaskTimer(this,0,100);}}
